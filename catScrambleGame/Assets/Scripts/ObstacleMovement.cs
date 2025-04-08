dafc8d8039a6a5272602dbf0f8f2847b38671978
Emily Herbert <emarieherb@gmail.com>
2025-04-08 08:20:54 -0700

    new buildings!

---
+++ catScrambleGame/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    public float speed = 50f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+
+        if (transform.position.z < -100f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
